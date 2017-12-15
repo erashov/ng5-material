2f7f0b0ef9157356c79daf774e0abe476154eb23
Alex <erashov@outlook.com>
2017-12-15 16:10:48 +0300

    initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  answer = '';
+  answerDisplay = '';
+  showSpinner = false;
+  showAnswer() {
+    this.showSpinner = true;
+    setTimeout(() => {
+      this.answerDisplay = this.answer;
+      this.showSpinner = false;
+    }, 2000);
+  }
+}
